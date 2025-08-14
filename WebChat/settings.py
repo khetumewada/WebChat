70c8618a81ace72166564bc9debb7d2e37b0e95a
khetu <khetu.mewada@gmail.com>
2025-08-14 16:28:27 +0530

    fix settings

--- WebChat/settings.py
+++ WebChat/settings.py
@@ -27,6 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://webchat-omwd.onrender.com'
+]
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 # Application definition
