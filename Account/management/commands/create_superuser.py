4d638f930f1b8235486b6089d716e46f61007448
khetu <khetu.mewada@gmail.com>
2025-08-15 15:43:15 +0530

    file fix of superuser

--- Account/management/commands/create_superuser.py
+++ Account/management/commands/create_superuser.py
@@ -1,13 +1,14 @@
 import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from decouple import config
 class Command(BaseCommand):
     help = "Creates a superuser from environment variables if one doesn't exist."
     def handle(self, *args, **options):
         User = get_user_model()
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        username = config("DJANGO_SUPERUSER_USERNAME")
+        email = config("DJANGO_SUPERUSER_EMAIL")
+        password = config("DJANGO_SUPERUSER_PASSWORD")
         if not username or not email or not password:
             self.stdout.write(self.style.ERROR(
                 "Environment variables DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL, and DJANGO_SUPERUSER_PASSWORD must be set."
